08776895d5d3f616cce7949f2a4e8dd2b30e18a8
hungnguyen50497 <nguyenhung50497@gmail.com>
2023-01-31 10:48:18 +0700

    doi ten

--- src/home.js
+++ src/home.js
@@ -19,46 +19,52 @@ function showList() {
                     <th scope="row">${item.id}</th>
                     <td>${item.name}</td>
                     <td>${item.price}</td>
-                    <td>${item.image}</td>
+                    <td><img src="${item.image}" alt="${item.image}" style="width: 200px;"></td>
                     <td>${item.nameCategory}</td>
                     <td>
                         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal${item.id}">Edit</button>
                         <div class="modal fade" id="editModal${item.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Edit ${item.name}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="input-group flex-nowrap">
-                                    <span class="input-group-text" id="addon-wrapping">Name</span>
-                                    <input type="text" class="form-control" id="name${item.id}" value="${item.name}" aria-label="Username" aria-describedby="addon-wrapping">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit ${item.name}</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                <br>
-                                <div class="input-group flex-nowrap">
-                                    <span class="input-group-text" id="addon-wrapping">Price</span>
-                                    <input type="text" class="form-control" id="price${item.id}" value="${item.price}" aria-label="Username" aria-describedby="addon-wrapping">
+                                <div class="modal-body">
+                                    <div class="input-group flex-nowrap">
+                                        <span class="input-group-text" id="addon-wrapping">Name</span>
+                                        <input type="text" class="form-control" id="name${item.id}" value="${item.name}" aria-label="Username" aria-describedby="addon-wrapping">
+                                    </div>
+                                    <br>
+                                    <div class="input-group flex-nowrap">
+                                        <span class="input-group-text" id="addon-wrapping">Price</span>
+                                        <input type="text" class="form-control" id="price${item.id}" value="${item.price}" aria-label="Username" aria-describedby="addon-wrapping">
+                                    </div>
+                                    <br>
+                                    <div class="input-group flex-nowrap">
+                                        <span class="input-group-text" id="addon-wrapping">Category</span>
+                                        <select id="category${item.id}" class="form-select" aria-label="Default select example" aria-describedby="addon-wrapping">
+                                            <option value="${item.idCategory}">${item.nameCategory}</option>
+                                            ${categories}
+                                        </select>
+                                    </div>
+                                    <br>
+                                    <div class="input-group flex-nowrap">
+                                        <span class="input-group-text" id="addon-wrapping">Image</span>
+                                        <input type="file" id="fileButton" onchange="uploadImageEdit(event, ${item.id})" class="form-control" placeholder="Image" aria-label="Username" aria-describedby="addon-wrapping">
+                                    </div>
+                                    <br>
+                                    <div class="input-group flex-nowrap">
+                                        <span class="input-group-text" id="addon-wrapping">@</span>
+                                        <div class="container-fluid" id="imgDiv${item.id}" aria-describedby="addon-wrapping"><img src="${item.image}" alt="${item.image}" style="width: 500px;"></div>
+                                    </div>
                                 </div>
-                                <br>
-                                <div class="input-group flex-nowrap">
-                                    <span class="input-group-text" id="addon-wrapping">Image</span>
-                                    <input type="text" class="form-control" id="image${item.id}" value="${item.image}" aria-label="Username" aria-describedby="addon-wrapping">
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="editProduct(${item.id})">Save changes</button>
                                 </div>
-                                <br>
-                                <div class="input-group flex-nowrap">
-                                    <select id="category${item.id}" class="form-select" aria-label="Default select example">
-                                        <option value="${item.idCategory}">${item.nameCategory}</option>
-                                        ${categories}
-                                    </select>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="editProduct(${item.id})">Save changes</button>
-                            </div>
-                            </div>
-                        </div>
                         </div>
                     </td>
                     <td>
@@ -115,30 +121,38 @@ function showFormAdd() {
         headers : {
             'Content-Type': 'application/json',
         },
-        success: () => {
+        success: (data) => {
+            let categories = ``;
+            data[1].map((item) => {
+                categories += `<option value="${item.idCategory}">${item.nameCategory}</option>`
+            })
             let html = `
-            <div class="input-group flex-nowrap">
-                <span class="input-group-text" id="addon-wrapping">Name</span>
-                <input type="text" class="form-control" id="name" placeholder="Name" aria-label="Username" aria-describedby="addon-wrapping">
-            </div>
-            <br>
-            <div class="input-group flex-nowrap">
-                <span class="input-group-text" id="addon-wrapping">Price</span>
-                <input type="text" class="form-control" id="price" placeholder="Price" aria-label="Username" aria-describedby="addon-wrapping">
-            </div>
-            <br>
-            <div class="input-group flex-nowrap">
-                <span class="input-group-text" id="addon-wrapping">Image</span>
-                <input type="text" class="form-control" id="image" placeholder="Image" aria-label="Username" aria-describedby="addon-wrapping">
-            </div>
-            <br>
-            <div class="input-group flex-nowrap">
-                <span class="input-group-text" id="addon-wrapping">Category</span>
-                <input type="text" class="form-control" id="category" placeholder="Category" aria-label="Username" aria-describedby="addon-wrapping">
-            </div>
-            <br>
-            <button class="btn btn-primary" onclick="addProduct()">Save</button>
-            `;
+                <div class="container-fluid">
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">Name</span>
+                        <input type="text" class="form-control" id="name" placeholder="Name" aria-label="Username" aria-describedby="addon-wrapping">
+                    </div>
+                    <br>
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">Price</span>
+                        <input type="text" class="form-control" id="price" placeholder="Price" aria-label="Username" aria-describedby="addon-wrapping">
+                    </div>
+                    <br>
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">Category</span>
+                        <select id="category" class="form-select" aria-label="Default select example" aria-describedby="addon-wrapping">
+                            ${categories}
+                        </select>
+                    </div>
+                    <br>
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">Image</span>
+                        <input type="file" id="fileButton" onchange="uploadImage(event)" class="form-control" placeholder="Image" aria-label="Username" aria-describedby="addon-wrapping">
+                    </div>
+                    <div id="imgDiv"></div>
+                    <br>
+                    <button class="btn btn-primary" onclick="addProduct()">Save</button>
+                </div>`;
             $('#body').html(html);
         }
     })
@@ -147,7 +161,7 @@ function showFormAdd() {
 function addProduct() {
     let name = $('#name').val();
     let price = $('#price').val();
-    let image = $('#image').val();
+    let image = localStorage.getItem('image');
     let category = $('#category').val();
     let product = {
         name: name,
@@ -187,7 +201,7 @@ function deleteProduct(id) {
 function editProduct(id) {
     let name = $(`#name${id}`).val();
     let price = $(`#price${id}`).val();
-    let image = $(`#image${id}`).val();
+    let image = localStorage.getItem('image');
     let category = $(`#category${id}`).val();
     let product = {
         name: name,
@@ -208,4 +222,66 @@ function editProduct(id) {
             showHome();
         }
     })
+}
+
+function uploadImage(e) {
+    let fbBucketName = 'images';
+    let uploader = document.getElementById('uploader');
+    let file = e.target.files[0];
+    let storageRef = firebase.storage().ref(`${fbBucketName}/${file.name}`);
+    let uploadTask = storageRef.put(file);
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        function (snapshot) {
+            uploader.value = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED:
+                    break;
+                case firebase.storage.TaskState.RUNNING:
+                    break;
+            }
+        }, function (error) {
+            switch (error.code) {
+                case 'storage/unauthorized':
+                    break;
+                case 'storage/canceled':
+                    break;
+                case 'storage/unknown':
+                    break;
+            }
+        }, function () {
+            let downloadURL = uploadTask.snapshot.downloadURL;
+            document.getElementById('imgDiv').innerHTML = `<img src="${downloadURL}" alt="${downloadURL}"  style="width: 500px;" id="image">`
+            localStorage.setItem('image', downloadURL);
+        });
+}
+
+function uploadImageEdit(e, id) {
+    let fbBucketName = 'images';
+    let uploader = document.getElementById('uploader');
+    let file = e.target.files[0];
+    let storageRef = firebase.storage().ref(`${fbBucketName}/${file.name}`);
+    let uploadTask = storageRef.put(file);
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        function (snapshot) {
+            uploader.value = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED:
+                    break;
+                case firebase.storage.TaskState.RUNNING:
+                    break;
+            }
+        }, function (error) {
+            switch (error.code) {
+                case 'storage/unauthorized':
+                    break;
+                case 'storage/canceled':
+                    break;
+                case 'storage/unknown':
+                    break;
+            }
+        }, function () {
+            let downloadURL = uploadTask.snapshot.downloadURL;
+            document.getElementById(`imgDiv${id}`).innerHTML = `<img src="${downloadURL}" alt="${downloadURL}"  style="width: 500px;">`
+            localStorage.setItem('image', downloadURL);
+        });
 }
